3ffcb37afbaab647b8e460d52c9a01c08e033273
yaakgold <yaakovgoldberg007@gmail.com>
2020-06-29 09:50:38 -0400

    Setup Core for the project

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -1,20 +1,26 @@
 // Game.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include "core.h"
 #include <iostream>
+
+bool Update(float dt)
+{
+    return false;
+}
+
+void Draw(Core::Graphics& graphics)
+{
+    graphics.SetColor(RGB(rand() % 256, rand() % 256, rand() % 256));
+    graphics.DrawLine(static_cast<float>(rand() % 800), static_cast<float>(rand() % 600), static_cast<float>(rand() % 800), static_cast<float>(rand() % 600));
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    char name[] = "CSC196"; 
+    Core::Init(name, 800, 600); 
+    Core::RegisterUpdateFn(Update); 
+    Core::RegisterDrawFn(Draw); 
+    Core::GameLoop();
+    Core::Shutdown();
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
