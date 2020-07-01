3fe9bf799d238f759ea97ed5a34d28313c6152d7
yaakgold <yaakovgoldberg007@gmail.com>
2020-07-01 13:55:51 -0400

    Day 3: Adding more functionality to Vector2. Including normalizing the vectors

--- Engine/Math/Vector2.h
+++ Engine/Math/Vector2.h
@@ -10,17 +10,86 @@ namespace hum
 		Vector2() : x{ 0 }, y{ 0 } {}
 
 		Vector2(float x, float y) : x{ x }, y{ y } {}
+		Vector2(int x, int y) : x{ static_cast<float>(x) }, y{ static_cast<float>(y) } {}
 
-		void set(float x, float y) { this->x = x; this->y = y; }
+		void Set(float x, float y) { this->x = x; this->y = y; }
 
+#pragma region Operator Overloads
+
+		//Two Vector2 objs
 		Vector2 operator + (const Vector2& v) const { return Vector2{ x + v.x, y + v.y }; }
 		Vector2 operator - (const Vector2& v) const { return Vector2{ x - v.x, y - v.y }; }
 		Vector2 operator * (const Vector2& v) const { return Vector2{ x * v.x, y * v.y }; }
 		Vector2 operator / (const Vector2& v) const { return Vector2{ x / v.x, y / v.y }; }
 
+		Vector2& operator += (const Vector2& v) { x += v.x; y += v.y; return *this; }
+		Vector2& operator -= (const Vector2& v) { x -= v.x; y -= v.y; return *this; }
+		Vector2& operator *= (const Vector2& v) { x *= v.x; y *= v.y; return *this; }
+		Vector2& operator /= (const Vector2& v) { x /= v.x; y /= v.y; return *this; }
+
+		//Vector2 obj with scalar
 		Vector2 operator + (float s) const { return Vector2{ x + s, y + s }; }
 		Vector2 operator - (float s) const { return Vector2{ x - s, y - s }; }
 		Vector2 operator * (float s) const { return Vector2{ x * s, y * s }; }
 		Vector2 operator / (float s) const { return Vector2{ x / s, y / s }; }
+
+		Vector2& operator += (const float s) { x += s; y += s; return *this; }
+		Vector2& operator -= (const float s) { x -= s; y -= s; return *this; }
+		Vector2& operator *= (const float s) { x *= s; y *= s; return *this; }
+		Vector2& operator /= (const float s) { x /= s; y /= s; return *this; }
+
+		Vector2 operator - () { return Vector2{ -x, -y }; }
+
+#pragma endregion
+
+
+#pragma region Method Declaration
+		float Length() const;
+		float LengthSqr() const;
+		
+		Vector2 Normalized() const;
+		void Normalize();
+
+		static float Distance(const Vector2& v1, const Vector2& v2);
+
+#pragma endregion
 	};
+
+#pragma region Method Implementation
+	float Vector2::Length() const
+	{
+		return std::sqrt((x * x) + (y * y));
+	}
+
+	float Vector2::LengthSqr() const
+	{
+		return (x * x) + (y * y);
+	}
+
+	float Vector2::Distance(const Vector2& v1, const Vector2& v2)
+	{
+		Vector2 v = v1 - v2;
+		return v.Length();
+	}
+
+	Vector2 Vector2::Normalized() const
+	{
+		float length = Length();
+		Vector2 n = (length == 0.0f) ? Vector2{ 0.0f, 0.0f } : *this / length;
+		return n;
+	}
+
+	void Vector2::Normalize()
+	{
+		float length = Length();
+		if (length == 0.0f)
+		{
+			x = y = 0.0f;
+		}
+		else
+		{
+			*this /= length;
+		}
+	}
+#pragma endregion
 }
--- Game/Game.cpp
+++ Game/Game.cpp
@@ -1,30 +1,49 @@
-// Game.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include "core.h"
 #include "Math/Math.h"
 #include "Math/Random.h"
 #include "Math/Vector2.h"
 #include <iostream>
 
-hum::Vector2 position{ 400, 300 };
+const size_t NUM_POINTS = 40;
+float speed = 5;
+float scale = 1;
 std::vector< hum::Vector2> points;
+hum::Vector2 position{ 400.0f, 300.0f };
 
 
 bool Update(float dt)
 {
-    return false;
+    bool quit = Core::Input::IsPressed(Core::Input::KEY_ESCAPE);
+
+    int x, y;
+    Core::Input::GetMousePos(x, y);
+
+    hum::Vector2 target = hum::Vector2(x, y);
+    hum::Vector2 direction = target - position;
+
+    position += direction.Normalized() * speed;
+
+    //if (Core::Input::IsPressed(Core::Input::KEY_LEFT) || Core::Input::IsPressed('A')) { position += hum::Vector2{ -1.0f, 0.0f } * speed; }
+    //if (Core::Input::IsPressed(Core::Input::KEY_RIGHT) || Core::Input::IsPressed('D')) { position += hum::Vector2{ 1.0f, 0.0f } * speed; }
+    //if (Core::Input::IsPressed(Core::Input::KEY_UP) || Core::Input::IsPressed('W')) { position += hum::Vector2{ 0.0f, -1.0f } * speed; }
+    //if (Core::Input::IsPressed(Core::Input::KEY_DOWN) || Core::Input::IsPressed('S')) { position += hum::Vector2{ 0.0f, 1.0f } * speed; }
+
+    for (hum::Vector2& point : points)
+    {
+        point = { hum::random(-10.0f, 10.0f), hum::random(-10.0f, 10.0f) };
+    }
+
+    return quit;
 }
 
 void Draw(Core::Graphics& graphics)
 {
     graphics.SetColor(RGB(rand() % 256, rand() % 256, rand() % 256));
-    //graphics.DrawLine(static_cast<float>(rand() % 800), static_cast<float>(rand() % 600), static_cast<float>(rand() % 800), static_cast<float>(rand() % 600));
     
-    for (size_t i = 0; i < 40 - 1; i++)
+    for (size_t i = 0; i < NUM_POINTS - 1; i++)
     {
-        hum::Vector2 p1 = points[i];
-        hum::Vector2 p2 = points[i + 1];
+        hum::Vector2 p1 = position + (points[i] * scale);
+        hum::Vector2 p2 = position + (points[i + 1] * scale);
 
         graphics.DrawLine(p1.x, p1.y, p2.x, p2.y);
     }
