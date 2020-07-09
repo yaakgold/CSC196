b5282561f54f383803da6b1937950c415376818e
yaakgold <yaakovgoldberg007@gmail.com>
2020-07-09 13:44:29 -0400

    Day 8: Updated the MathFile, added more constants, and the Lerp function

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -13,9 +13,7 @@ using namespace hummus;
 float speed = 350;
 float rotSpeed = 3;
 
-Shape ship;
-
-Transform transform{ {400, 300}, 4, 0 };
+float t{ 0 };
 
 float frameTime;
 float roundTime{ 0 };
@@ -33,13 +31,14 @@ bool Update(float dt)
     deltaTime = time - prevTime;
     prevTime = time;
     
+    t += (dt * 5.0f);
     
     frameTime = dt;
     roundTime += dt;
 
     if (roundTime >= 5.0f) gameOver = true;
 
-    if (gameOver) dt = 0.0f;
+    //if (gameOver) dt = 0.0f;
     
     //dt *= 2.0f;
 
@@ -63,6 +62,8 @@ bool Update(float dt)
     if (Core::Input::IsPressed(Core::Input::KEY_LEFT) || Core::Input::IsPressed('A')) { player.GetTransform().angle -= dt * rotSpeed; }
     if (Core::Input::IsPressed(Core::Input::KEY_RIGHT) || Core::Input::IsPressed('D')) { player.GetTransform().angle += dt * rotSpeed; }
 
+    player.GetTransform().position = Clamp(player.GetTransform().position, { 0, 0 }, { 800, 600 });
+
     //if (Core::Input::IsPressed(Core::Input::KEY_LEFT) || Core::Input::IsPressed('A')) { position += Vector2::left * speed * dt; }
     //if (Core::Input::IsPressed(Core::Input::KEY_RIGHT) || Core::Input::IsPressed('D')) { position += Vector2::right * speed * dt; }
     
@@ -76,6 +77,13 @@ void Draw(Core::Graphics& graphics)
     graphics.DrawString(10, 30, std::to_string(1.0f / frameTime).c_str());
     graphics.DrawString(10, 50, std::to_string(deltaTime / 1000.0f).c_str());
 
+    float v = (std::sin(t) + 1.0f) * 0.5f;
+
+    Vector2 p = Lerp(Vector2{ 400, 300 }, Vector2{ 600, 0 }, v);
+    Color c = Lerp(Color{ 0, 0, 1}, Color{1, 0, 0 }, v);
+    graphics.SetColor(c);
+    graphics.DrawString(p.x, p.y, "Title or something like that");
+
     if (gameOver) graphics.DrawString(400, 300, "Game Over");
 
     player.Draw(graphics);
@@ -86,7 +94,6 @@ int main()
 {
     prevTime = GetTickCount();
 
-    ship.Load("ship.txt");
     player.Load("player.txt");
     enemy.Load("enemy.txt");
 
